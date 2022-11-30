7b28e9a9e37b9ec34934505b5c599e300a0461fd
DHSNIEL <sukidi.dh@gmail.com>
2022-11-30 18:17:36 +0700

    Update Employee 2
    Melengkapi Seluruh Methods

--- BankDiba_LIB/Employee.cs
+++ BankDiba_LIB/Employee.cs
@@ -54,14 +54,14 @@ namespace BankDiba_LIB
             string sql = "";
             if (kriteria == "")
             {
-                sql = $"select E.KodeEmployee, E.Nama, E.TglLahir, E.Alamat, E.Gaji, E.Username, E.Password" +
-                      $"P.id, P.nama AS Positions from Employee E inner join Positions P on E.position_id = P.id";
+                sql = $"select E.id, E.nama_depan, E.nama_keluarga, E.position_id, E.nik, E.email, E.password, E.tgl_buat, E.tgl_perubahan" +
+                      $", P.id, P.nama AS Positions from Employee E inner join Positions P on E.position_id = P.id";
             }
             else
             {
-                sql = $"select E.KodeEmployee, E.Nama, E.TglLahir, E.Alamat, E.Gaji, E.Username, E.Password" +
-                      $"P.id, P.nama AS Positions from Employee E inner join Positions P on E.position_id = P.id" +
-                      $"where {kriteria} LIKE '%{nilaiKriteria}%'";
+                sql = $"select E.id, E.nama_depan, E.nama_keluarga, E.position_id, E.nik, E.email, E.password, E.tgl_buat, E.tgl_perubahan" +
+                      $", P.id, P.nama AS Positions from Employee E inner join Positions P on E.position_id = P.id" +
+                $"where {kriteria} LIKE '%{nilaiKriteria}%'";
             }
             // ------
             MySqlDataReader hasil = Koneksi.JalankanPerintahQuery(sql);
@@ -76,6 +76,85 @@ namespace BankDiba_LIB
                 listEmployee.Add(E);
             }
             return listEmployee;
+        }
+        public static Employee CekLogin(string user, string passwd)
+        {
+            string sql = "";
+            sql = $"select E.id, E.nama_depan, E.nama_keluarga, E.position_id, E.nik, E.email, E.password, E.tgl_buat, E.tgl_perubahan" +
+                  $", P.id, P.nama AS Positions from Employee E inner join Positions P on E.position_id = P.id" +
+                  $"where email='{user}' AND password=SHA2('{passwd}', 512)";
+
+            MySqlDataReader hasil = Koneksi.JalankanPerintahQuery(sql);
+
+            while (hasil.Read() == true)
+            {
+                Positions p = new Positions((int)hasil.GetValue(0), hasil.GetString(1), hasil.GetString(2));
+                Employee e = new Employee(int.Parse(hasil.GetString(0)), hasil.GetString(1), hasil.GetString(2),
+                                           p, hasil.GetString(4), hasil.GetString(5),
+                                           hasil.GetString(6), hasil.GetDateTime(7), hasil.GetDateTime(8));
+                return e;
+            }
+            return null;
+        }
+        public static void TambahData(Employee e)
+        {
+            string sql = $"INSERT INTO employee (nama_depan, nama_keluarga, position_id, nik, email, password, tgl_buat, tgl_perubahan) " +
+                $"values ('{e.NamaDepan.Replace("'", "\\'")}', '{e.NamaKeluarga.Replace("'", "\\'")}', " +
+                $"'{e.Positions.Id}', '{e.Nik}', '{e.Email}', SHA2('{e.Password}', 512), " +
+                $"'{e.TglBuat.ToString("yyyy-MM-dd")}', '{e.TglPerubahan.ToString("yyyy-MM-dd")}')";
+            Koneksi.JalankanPerintahDML(sql);
+        }
+        public static Boolean UbahData(Employee e)
+        {
+            string sql = $"update employee set nama_depan ='{e.NamaDepan.Replace("'", "\\'")}, nama_keluarga='{e.NamaKeluarga.Replace("'", "\\")}'"+
+                $",position_id='{e.Positions.Id}'"+
+                $",nik=' {e.Nik} '"+
+                $",email=' {e.Email} '"+
+                $",password=SHA2('{e.Password}', 512)" +
+                $",tgl_buat='{e.TglBuat}'" +
+                $",tgl_perubahan='{e.TglPerubahan}'" +
+                $"where id='{e.Id}'";
+            int jumlahDataBerubah = Koneksi.JalankanPerintahDML(sql);
+            if (jumlahDataBerubah == 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        public static Boolean HapusData(Employee e)
+        {
+            string sql = "delete from employee where id = '" +
+                e.Id+ "'";
+            int jumlahDataBerubah = Koneksi.JalankanPerintahDML(sql);
+            if (jumlahDataBerubah == 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        public static string GenerateKode() // generate id
+        {
+            //dapatkan kode pegawai terakhir (kode terbesar)
+            string sql = "select max(id) from employee";
+            string hasilKode = "";
+            MySqlDataReader hasil = Koneksi.JalankanPerintahQuery(sql);
+            if (hasil.Read() == true)
+            {
+                int kodeTerbaru = int.Parse(hasil.GetString(0)) + 1;
+                hasilKode = kodeTerbaru.ToString();
+            }
+            else
+            {
+                //jika belum ada data maka kode pegawai diset 1
+                hasilKode = "1";
+            }
+            return hasilKode;
         }
         #endregion
     }
